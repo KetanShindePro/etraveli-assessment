f30efe8bc3dfcf094cac9048eeab0075a9c9d5b2
KetanShindePro <ketanshinde.pro@gmail.com>
2025-05-18 21:44:23 +0530

    Linked in link added in footer

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import "./footer.css";
+import { CiLinkedin } from "react-icons/ci";
 
 export default function Footer() {
   return (
     <footer className="layout-footer">
       <span className="footer-logo">MoviePedia</span>
-      <span className="footer-content">Social Media Links</span>
+      <span className="footer-content">
+        <a
+          href="https://www.linkedin.com/in/ketan-shinde-47676a70/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <CiLinkedin size={"3rem"} />
+        </a>
+      </span>
       <span className="footer-copywrite">@Copywrite by MoviePedia</span>
     </footer>
   );
